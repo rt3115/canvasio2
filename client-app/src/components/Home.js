25562a2eb202a1c77d282cd38483148637ac16f8
rt3115 <43387338+rt3115@users.noreply.github.com>
2020-11-11 19:59:01 -0500

    style stuff

--- client-app/src/components/Home.js
+++ client-app/src/components/Home.js
@@ -1,26 +1,27 @@
 import React, { Component } from 'react';
 import { Button, ButtonGroup } from '@material-ui/core';
-import '../styles/home.css'
-import '../styles/w3.css'
+import '../styles/home.css';
+import '../styles/w3.css';
 import background from "../refs/background.jpg";
 import Chat from './chat/Chat';
 
 var sectionStyle = {
-    width: "400%",
-    height: "800px",
+    width: "100%",
+    height: "100%",
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'auto',
     backgroundImage: `url(${background})`
 
 };
 
 export const Home = ( { children } ) => {
     return (
-        <section style={sectionStyle}>
-        <div class="w3-display-middle">
+        <div id="home-section">
             <head>
                 <title>Homepage</title>
                 <link rel="stylesheet" href="refs/Home.css" />
             </head>
-            <body>
+            <body className="cluster">
                 <h1>Canvas.io</h1>
                 <Button type="submit" href="#/register">
                     REGISTER
@@ -29,8 +30,7 @@ export const Home = ( { children } ) => {
                     LOGIN
                 </Button>
             </body>
-          
-            </div>
-        </section>
+
+        </div>
     );
   }
